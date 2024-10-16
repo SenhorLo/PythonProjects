4f9dea93eb4d08205343a121140295034951831f
Lorenzo Tacca Orssatto <154294191+SenhorLo@users.noreply.github.com>
2024-10-16 19:37:29 -0300

    Reverse String without reverse metod

---
+++ reverseString.py
@@ -0,0 +1,12 @@
+
+original_string = str(input("something..."))
+
+
+reverse_string = ""
+
+
+for caracter in original_string:
+    reverse_string = caracter + reverse_string  
+
+
+print(reverse_string)
